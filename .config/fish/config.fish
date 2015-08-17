e60630933790d0380e76956be763d51f1ba6fa3b
Sam Rang <srang2010@gmail.com>
2015-08-16 21:11:43 -0500

    Sun Aug 16 2015 21:11:43 GMT-0500 (CDT)

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+set -x EDITOR /usr/local/bin/vim
+set -x TZ 'America/Chicago'
+set fish_color_user               00FF00
+set fish_color_time               3333FF
+set fish_color_git                0033FF
+set fish_color_normal             660066
+set fish_color_command            33CCFF
+set fish_color_quote              0099FF
+set fish_color_redirection        66FF99
+set fish_color_end                993300
+set fish_color_error              FF0000
+set fish_color_param              99FF00
+set fish_color_comment            505050
+set fish_color_match              CC6666
+set fish_color_search_match       CCFF00
+set fish_color_operator           3333FF
+set fish_color_escape             333333
+set fish_color_cwd                33FF33
+set fish_pager_color_prefix       006600
+set fish_pager_color_completion   006633
+set fish_pager_color_description  003300
+set fish_pager_color_progress     000066
+set fish_pager_color_secondary    000099
+set -gx PATH $PATH /usr/local/games
