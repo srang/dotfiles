18b1086c21e6c2664ddc2901c3bfd68786074e9a
Sam Rang <srang2010@gmail.com>
2021-06-14 11:00:33 -0400

    cleanup

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#          _                    _
+# ___  ___| |_ _   _ _ __   ___| |__
+#/ __|/ _ \ __| | | | '_ \ / __| '_ \
+#\__ \  __/ |_| |_| | |_) |\__ \ | | |
+#|___/\___|\__|\__,_| .__(_)___/_| |_|
+#                   |_|
+
+# check OS
+
+### MAC
+
+# check if homebrew installed
+# confirm default shell
+# brew install ansible
+# ansible-playbook configure-local.yaml
